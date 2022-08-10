1d592bdbecff7081e85c5d03711d7793d72d4d1c
Alex Forshtat <forshtat1@gmail.com>
2022-08-10 21:26:50 +0200

    Load hardhat fixture in test

---
+++ packages/hardhat/deploy/deploy-helper.ts
@@ -0,0 +1,19 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment): Promise<void | boolean> {
+  const { deploy } = hre.deployments
+  const [deployer] = await hre.ethers.provider.listAccounts()
+  if (!deployer) {
+    throw new Error('no deployer. missing MNEMONIC_FILE ?')
+  }
+  await deploy('BundlerHelper', {
+    from: deployer,
+    args: [],
+    log: true,
+    deterministicDeployment: true
+  })
+}
+
+export default func
+func.tags = ['BundlerHelper']
