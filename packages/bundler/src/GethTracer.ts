e3c8c8f1cb27105a384345c77ed49d69ed9efb32
Dror Tirosh <dror@opengsn.org>
2023-02-20 19:39:16 +0200

    lints

--- packages/bundler/src/GethTracer.ts
+++ packages/bundler/src/GethTracer.ts
@@ -54,7 +54,7 @@ export function getTracerBodyString (func: LogTracerFunc): string {
   ret = ret
     // .replace(/\/\/.*\n/g,'\n')
     // .replace(/\n\s*\n/g, '\n')
-    .replace(/\b(?:const|let)\b/g,'')
+    .replace(/\b(?:const|let)\b/g, '')
   // console.log('== tracer source',ret.split('\n').map((line,index)=>`${index}: ${line}`).join('\n'))
   return ret
 }
