bdc421b536b3209cb2fa905f02ec2061d318450a
Dror Tirosh <dror.tirosh@gmail.com>
2023-04-30 17:00:03 +0300

    Api v0.5 (#97)
    * runop --auto, --selfBundler, exit on finish.
    * update ep address
    * fund address on testnet

--- packages/bundler/src/runner/runop.ts
+++ packages/bundler/src/runner/runop.ts
@@ -129,7 +129,7 @@ async function main (): Promise<void> {
       })
     }
 
-    const argv = ['node', 'exec', '--config', './localconfig/bundler.config.json', '--unsafe']
+    const argv = ['node', 'exec', '--config', './localconfig/bundler.config.json', '--unsafe', '--auto']
     if (opts.entryPoint != null) {
       argv.push('--entryPoint', opts.entryPoint)
     }
@@ -193,3 +193,5 @@ async function main (): Promise<void> {
 }
 
 void main()
+  .catch(e => { console.log(e); process.exit(1) })
+  .then(() => process.exit(0))
