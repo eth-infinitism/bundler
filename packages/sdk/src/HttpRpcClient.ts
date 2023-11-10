d50c1b4d7ae2b6dc16d0a9e885b1cd7f3b98524e
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-11-10 17:23:47 +0530

    Added getting valid nonce key

--- packages/sdk/src/HttpRpcClient.ts
+++ packages/sdk/src/HttpRpcClient.ts
@@ -1,36 +1,50 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
-import { ethers } from 'ethers'
-import { resolveProperties } from 'ethers/lib/utils'
-import { UserOperationStruct } from '@account-abstraction/contracts'
-import Debug from 'debug'
-import { deepHexlify } from '@account-abstraction/utils'
-import { AdvancedUserOperationStruct, AdvancedUserOperations } from './AdvancedUserOp'
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import { resolveProperties } from "ethers/lib/utils";
+import { UserOperationStruct } from "@account-abstraction/contracts";
 
-const debug = Debug('aa.rpc')
+import Debug from "debug";
+import {
+  NotPromise,
+  deepHexlify,
+  getUserOpHashWithoutNonce,
+} from "@account-abstraction/utils";
+
+import {
+  AdvancedUserOperationStruct,
+  AdvancedUserOperations,
+} from "./AdvancedUserOp";
+
+const debug = Debug("aa.rpc");
 
 export class HttpRpcClient {
-  private readonly userOpJsonRpcProvider: JsonRpcProvider
+  private readonly userOpJsonRpcProvider: JsonRpcProvider;
 
-  initializing: Promise<void>
+  initializing: Promise<void>;
 
   constructor(
     readonly bundlerUrl: string,
     readonly entryPointAddress: string,
     readonly chainId: number
   ) {
-    this.userOpJsonRpcProvider = new ethers.providers.JsonRpcProvider(this.bundlerUrl, {
-      name: 'Connected bundler network',
-      chainId
-    })
-    this.initializing = this.validateChainId()
+    this.userOpJsonRpcProvider = new ethers.providers.JsonRpcProvider(
+      this.bundlerUrl,
+      {
+        name: "Connected bundler network",
+        chainId,
+      }
+    );
+    this.initializing = this.validateChainId();
   }
 
   async validateChainId(): Promise<void> {
     // validate chainId is in sync with expected chainid
-    const chain = await this.userOpJsonRpcProvider.send('eth_chainId', [])
-    const bundlerChain = parseInt(chain)
+    const chain = await this.userOpJsonRpcProvider.send("eth_chainId", []);
+    const bundlerChain = parseInt(chain);
     if (bundlerChain !== this.chainId) {
-      throw new Error(`bundler ${this.bundlerUrl} is on chainId ${bundlerChain}, but provider is on chainId ${this.chainId}`)
+      throw new Error(
+        `bundler ${this.bundlerUrl} is on chainId ${bundlerChain}, but provider is on chainId ${this.chainId}`
+      );
     }
   }
 
@@ -39,13 +53,41 @@ export class HttpRpcClient {
    * @param userOp1
    * @return userOpHash the id of this operation, for getUserOperationTransaction
    */
-  async sendUserOpToBundler(userOp1: AdvancedUserOperationStruct): Promise<string> {
-    await this.initializing
-    const hexifiedUserOp = deepHexlify(await resolveProperties(userOp1))
-    const jsonRequestData: [UserOperationStruct, string] = [hexifiedUserOp, this.entryPointAddress]
-    await this.printUserOperation('eth_sendUserOperation', jsonRequestData)
-    return await this.userOpJsonRpcProvider
-      .send('eth_sendUserOperation', [hexifiedUserOp, this.entryPointAddress])
+  async sendUserOpToBundler(
+    userOp1: AdvancedUserOperationStruct
+  ): Promise<string> {
+    await this.initializing;
+    const hexifiedUserOp = deepHexlify(await resolveProperties(userOp1));
+    const jsonRequestData: [UserOperationStruct, string] = [
+      hexifiedUserOp,
+      this.entryPointAddress,
+    ];
+    await this.printUserOperation("eth_sendUserOperation", jsonRequestData);
+    return await this.userOpJsonRpcProvider.send("eth_sendUserOperation", [
+      hexifiedUserOp,
+      this.entryPointAddress,
+    ]);
+  }
+
+  /**
+   * send a UserOperation to the bundler
+   * @param userOp1
+   * @return userOpHash the id of this operation, for getUserOperationTransaction
+   */
+  async getValidNonceKey(
+    userOp: NotPromise<UserOperationStruct>
+  ): Promise<string> {
+    const address = userOp.sender;
+    const userOpHash = getUserOpHashWithoutNonce(
+      userOp,
+      this.entryPointAddress,
+      this.chainId
+    );
+    await this.initializing;
+    return await this.userOpJsonRpcProvider.send("eth_getValidNonceKey", [
+      address,
+      userOpHash,
+    ]);
   }
 
   /**
@@ -53,21 +95,41 @@ export class HttpRpcClient {
    * @param userOp1
    * @returns latest gas suggestions made by the bundler.
    */
-  async estimateUserOpGas(userOp1: Partial<UserOperationStruct>): Promise<{ callGasLimit: number, preVerificationGas: number, verificationGasLimit: number }> {
-    await this.initializing
-    const hexifiedUserOp = deepHexlify(await resolveProperties(userOp1))
-    const jsonRequestData: [UserOperationStruct, string] = [hexifiedUserOp, this.entryPointAddress]
-    await this.printUserOperation('eth_estimateUserOperationGas', jsonRequestData)
-    return await this.userOpJsonRpcProvider
-      .send('eth_estimateUserOperationGas', [hexifiedUserOp, this.entryPointAddress])
+  async estimateUserOpGas(userOp1: Partial<UserOperationStruct>): Promise<{
+    callGasLimit: number;
+    preVerificationGas: number;
+    verificationGasLimit: number;
+  }> {
+    await this.initializing;
+    const hexifiedUserOp = deepHexlify(await resolveProperties(userOp1));
+    const jsonRequestData: [UserOperationStruct, string] = [
+      hexifiedUserOp,
+      this.entryPointAddress,
+    ];
+    await this.printUserOperation(
+      "eth_estimateUserOperationGas",
+      jsonRequestData
+    );
+    return await this.userOpJsonRpcProvider.send(
+      "eth_estimateUserOperationGas",
+      [hexifiedUserOp, this.entryPointAddress]
+    );
   }
 
-  private async printUserOperation(method: string, [userOp1, entryPointAddress]: [UserOperationStruct, string]): Promise<void> {
-    const userOp = await resolveProperties(userOp1)
-    debug('sending', method, {
-      ...userOp
-      // initCode: (userOp.initCode ?? '').length,
-      // callData: (userOp.callData ?? '').length
-    }, entryPointAddress)
+  private async printUserOperation(
+    method: string,
+    [userOp1, entryPointAddress]: [UserOperationStruct, string]
+  ): Promise<void> {
+    const userOp = await resolveProperties(userOp1);
+    debug(
+      "sending",
+      method,
+      {
+        ...userOp,
+        // initCode: (userOp.initCode ?? '').length,
+        // callData: (userOp.callData ?? '').length
+      },
+      entryPointAddress
+    );
   }
 }
--- packages/utils/src/ERC4337Utils.ts
+++ packages/utils/src/ERC4337Utils.ts
@@ -1,23 +1,106 @@
-import { defaultAbiCoder, hexConcat, hexlify, keccak256, resolveProperties } from 'ethers/lib/utils'
-import { UserOperationStruct } from '@account-abstraction/contracts'
-import { abi as entryPointAbi } from '@account-abstraction/contracts/artifacts/IEntryPoint.json'
-import { ethers } from 'ethers'
-import Debug from 'debug'
+import {
+  defaultAbiCoder,
+  hexConcat,
+  hexlify,
+  keccak256,
+  resolveProperties,
+} from "ethers/lib/utils";
+import { UserOperationStruct } from "@account-abstraction/contracts";
+import { abi as entryPointAbi } from "@account-abstraction/contracts/artifacts/IEntryPoint.json";
+import { ethers } from "ethers";
+import Debug from "debug";
 
-const debug = Debug('aa.utils')
+const debug = Debug("aa.utils");
 
 // UserOperation is the first parameter of validateUseOp
-const validateUserOpMethod = 'simulateValidation'
-const UserOpType = entryPointAbi.find(entry => entry.name === validateUserOpMethod)?.inputs[0]
+const validateUserOpMethod = "simulateValidation";
+const UserOpType = entryPointAbi.find(
+  (entry) => entry.name === validateUserOpMethod
+)?.inputs[0];
 if (UserOpType == null) {
-  throw new Error(`unable to find method ${validateUserOpMethod} in EP ${entryPointAbi.filter(x => x.type === 'function').map(x => x.name).join(',')}`)
+  throw new Error(
+    `unable to find method ${validateUserOpMethod} in EP ${entryPointAbi
+      .filter((x) => x.type === "function")
+      .map((x) => x.name)
+      .join(",")}`
+  );
 }
 
-export const AddressZero = ethers.constants.AddressZero
+export const AddressZero = ethers.constants.AddressZero;
 
 // reverse "Deferrable" or "PromiseOrValue" fields
 export type NotPromise<T> = {
-  [P in keyof T]: Exclude<T[P], Promise<any>>
+  [P in keyof T]: Exclude<T[P], Promise<any>>;
+};
+
+/**
+ * pack the userOperation
+ * @param op
+ * @param forSignature "true" if the hash is needed to calculate the getUserOpHash()
+ *  "false" to pack entire UserOp, for calculating the calldata cost of putting it on-chain.
+ */
+export function packUserOp(
+  op: NotPromise<UserOperationStruct>,
+  forSignature = true
+): string {
+  if (forSignature) {
+    return defaultAbiCoder.encode(
+      [
+        "address",
+        "uint256",
+        "bytes32",
+        "bytes32",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "bytes32",
+      ],
+      [
+        op.sender,
+        op.nonce,
+        keccak256(op.initCode),
+        keccak256(op.callData),
+        op.callGasLimit,
+        op.verificationGasLimit,
+        op.preVerificationGas,
+        op.maxFeePerGas,
+        op.maxPriorityFeePerGas,
+        keccak256(op.paymasterAndData),
+      ]
+    );
+  } else {
+    // for the purpose of calculating gas cost encode also signature (and no keccak of bytes)
+    return defaultAbiCoder.encode(
+      [
+        "address",
+        "uint256",
+        "bytes",
+        "bytes",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "bytes",
+        "bytes",
+      ],
+      [
+        op.sender,
+        op.nonce,
+        op.initCode,
+        op.callData,
+        op.callGasLimit,
+        op.verificationGasLimit,
+        op.preVerificationGas,
+        op.maxFeePerGas,
+        op.maxPriorityFeePerGas,
+        op.paymasterAndData,
+        op.signature,
+      ]
+    );
+  }
 }
 
 /**
@@ -26,24 +109,63 @@ export type NotPromise<T> = {
  * @param forSignature "true" if the hash is needed to calculate the getUserOpHash()
  *  "false" to pack entire UserOp, for calculating the calldata cost of putting it on-chain.
  */
-export function packUserOp (op: NotPromise<UserOperationStruct>, forSignature = true): string {
+export function packUserOpWithoutNonce(
+  op: NotPromise<UserOperationStruct>,
+  forSignature = true
+): string {
   if (forSignature) {
     return defaultAbiCoder.encode(
-      ['address', 'uint256', 'bytes32', 'bytes32',
-        'uint256', 'uint256', 'uint256', 'uint256', 'uint256',
-        'bytes32'],
-      [op.sender, op.nonce, keccak256(op.initCode), keccak256(op.callData),
-        op.callGasLimit, op.verificationGasLimit, op.preVerificationGas, op.maxFeePerGas, op.maxPriorityFeePerGas,
-        keccak256(op.paymasterAndData)])
+      [
+        "address",
+        "bytes32",
+        "bytes32",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "bytes32",
+      ],
+      [
+        op.sender,
+        keccak256(op.initCode),
+        keccak256(op.callData),
+        op.callGasLimit,
+        op.verificationGasLimit,
+        op.preVerificationGas,
+        op.maxFeePerGas,
+        op.maxPriorityFeePerGas,
+        keccak256(op.paymasterAndData),
+      ]
+    );
   } else {
     // for the purpose of calculating gas cost encode also signature (and no keccak of bytes)
     return defaultAbiCoder.encode(
-      ['address', 'uint256', 'bytes', 'bytes',
-        'uint256', 'uint256', 'uint256', 'uint256', 'uint256',
-        'bytes', 'bytes'],
-      [op.sender, op.nonce, op.initCode, op.callData,
-        op.callGasLimit, op.verificationGasLimit, op.preVerificationGas, op.maxFeePerGas, op.maxPriorityFeePerGas,
-        op.paymasterAndData, op.signature])
+      [
+        "address",
+        "bytes",
+        "bytes",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "uint256",
+        "bytes",
+        "bytes",
+      ],
+      [
+        op.sender,
+        op.initCode,
+        op.callData,
+        op.callGasLimit,
+        op.verificationGasLimit,
+        op.preVerificationGas,
+        op.maxFeePerGas,
+        op.maxPriorityFeePerGas,
+        op.paymasterAndData,
+        op.signature,
+      ]
+    );
   }
 }
 
@@ -56,37 +178,73 @@ export function packUserOp (op: NotPromise<UserOperationStruct>, forSignature = 
  * @param entryPoint
  * @param chainId
  */
-export function getUserOpHash (op: NotPromise<UserOperationStruct>, entryPoint: string, chainId: number): string {
-  const userOpHash = keccak256(packUserOp(op, true))
+export function getUserOpHash(
+  op: NotPromise<UserOperationStruct>,
+  entryPoint: string,
+  chainId: number
+): string {
+  const userOpHash = keccak256(packUserOp(op, true));
   const enc = defaultAbiCoder.encode(
-    ['bytes32', 'address', 'uint256'],
-    [userOpHash, entryPoint, chainId])
-  return keccak256(enc)
+    ["bytes32", "address", "uint256"],
+    [userOpHash, entryPoint, chainId]
+  );
+  return keccak256(enc);
 }
 
-const ErrorSig = keccak256(Buffer.from('Error(string)')).slice(0, 10) // 0x08c379a0
-const FailedOpSig = keccak256(Buffer.from('FailedOp(uint256,string)')).slice(0, 10) // 0x220266b6
+/**
+ * calculate the userOpHash of a given userOperation.
+ * The userOpHash is a hash of all UserOperation fields, except the "signature" field.
+ * The entryPoint uses this value in the emitted UserOperationEvent.
+ * A wallet may use this value as the hash to sign (the SampleWallet uses this method)
+ * @param op
+ * @param entryPoint
+ * @param chainId
+ */
+export function getUserOpHashWithoutNonce(
+  op: NotPromise<UserOperationStruct>,
+  entryPoint: string,
+  chainId: number
+): string {
+  const userOpHash = keccak256(packUserOpWithoutNonce(op, true));
+  const enc = defaultAbiCoder.encode(
+    ["bytes32", "address", "uint256"],
+    [userOpHash, entryPoint, chainId]
+  );
+  return keccak256(enc);
+}
+
+const ErrorSig = keccak256(Buffer.from("Error(string)")).slice(0, 10); // 0x08c379a0
+const FailedOpSig = keccak256(Buffer.from("FailedOp(uint256,string)")).slice(
+  0,
+  10
+); // 0x220266b6
 
 interface DecodedError {
-  message: string
-  opIndex?: number
+  message: string;
+  opIndex?: number;
 }
 
 /**
  * decode bytes thrown by revert as Error(message) or FailedOp(opIndex,paymaster,message)
  */
-export function decodeErrorReason (error: string): DecodedError | undefined {
-  debug('decoding', error)
+export function decodeErrorReason(error: string): DecodedError | undefined {
+  debug("decoding", error);
   if (error.startsWith(ErrorSig)) {
-    const [message] = defaultAbiCoder.decode(['string'], '0x' + error.substring(10))
-    return { message }
+    const [message] = defaultAbiCoder.decode(
+      ["string"],
+      "0x" + error.substring(10)
+    );
+    return { message };
   } else if (error.startsWith(FailedOpSig)) {
-    let [opIndex, message] = defaultAbiCoder.decode(['uint256', 'string'], '0x' + error.substring(10))
-    message = `FailedOp: ${message as string}`
+    let [opIndex, message] = defaultAbiCoder.decode(
+      ["uint256", "string"],
+      "0x" + error.substring(10)
+    );
+    message = `FailedOp: ${message as string}`;
     return {
       message,
-      opIndex
-    }
+      opIndex,
+    };
   }
 }
 
@@ -96,55 +254,63 @@ export function decodeErrorReason (error: string): DecodedError | undefined {
  * tested on geth, hardhat-node
  * usage: entryPoint.handleOps().catch(decodeError)
  */
-export function rethrowError (e: any): any {
-  let error = e
-  let parent = e
+export function rethrowError(e: any): any {
+  let error = e;
+  let parent = e;
   if (error?.error != null) {
-    error = error.error
+    error = error.error;
   }
   while (error?.data != null) {
-    parent = error
-    error = error.data
+    parent = error;
+    error = error.data;
   }
-  const decoded = typeof error === 'string' && error.length > 2 ? decodeErrorReason(error) : undefined
+  const decoded =
+    typeof error === "string" && error.length > 2
+      ? decodeErrorReason(error)
+      : undefined;
   if (decoded != null) {
-    e.message = decoded.message
+    e.message = decoded.message;
 
     if (decoded.opIndex != null) {
       // helper for chai: convert our FailedOp error into "Error(msg)"
-      const errorWithMsg = hexConcat([ErrorSig, defaultAbiCoder.encode(['string'], [decoded.message])])
+      const errorWithMsg = hexConcat([
+        ErrorSig,
+        defaultAbiCoder.encode(["string"], [decoded.message]),
+      ]);
       // modify in-place the error object:
-      parent.data = errorWithMsg
+      parent.data = errorWithMsg;
     }
   }
-  throw e
+  throw e;
 }
 
 /**
  * hexlify all members of object, recursively
  * @param obj
  */
-export function deepHexlify (obj: any): any {
-  if (typeof obj === 'function') {
-    return undefined
+export function deepHexlify(obj: any): any {
+  if (typeof obj === "function") {
+    return undefined;
   }
-  if (obj == null || typeof obj === 'string' || typeof obj === 'boolean') {
-    return obj
-  } else if (obj._isBigNumber != null || typeof obj !== 'object') {
-    return hexlify(obj).replace(/^0x0/, '0x')
+  if (obj == null || typeof obj === "string" || typeof obj === "boolean") {
+    return obj;
+  } else if (obj._isBigNumber != null || typeof obj !== "object") {
+    return hexlify(obj).replace(/^0x0/, "0x");
   }
   if (Array.isArray(obj)) {
-    return obj.map(member => deepHexlify(member))
+    return obj.map((member) => deepHexlify(member));
   }
-  return Object.keys(obj)
-    .reduce((set, key) => ({
+  return Object.keys(obj).reduce(
+    (set, key) => ({
       ...set,
-      [key]: deepHexlify(obj[key])
-    }), {})
+      [key]: deepHexlify(obj[key]),
+    }),
+    {}
+  );
 }
 
 // resolve all property and hexlify.
 // (UserOpMethodHandler receives data from the network, so we need to pack our generated values)
-export async function resolveHexlify (a: any): Promise<any> {
-  return deepHexlify(await resolveProperties(a))
+export async function resolveHexlify(a: any): Promise<any> {
+  return deepHexlify(await resolveProperties(a));
 }
