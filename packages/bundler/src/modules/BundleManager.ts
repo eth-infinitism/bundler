8294b2d2160611e6d683995ecf0e16e8142ce59b
Alex Forshtat <forshtat1@gmail.com>
2024-07-21 11:37:02 +0200

    Remove the 'STOPSHIP' comment

--- packages/bundler/src/modules/BundleManager.ts
+++ packages/bundler/src/modules/BundleManager.ts
@@ -74,7 +74,6 @@ export class BundleManager {
         debug('sendNextBundle - no bundle to send')
       } else {
         const beneficiary = await this._selectBeneficiary()
-        // TODO: STOPSHIP
         const ret = await this.sendBundle(bundle as UserOperation[], beneficiary, storageMap)
         debug(`sendNextBundle exit - after sent a bundle of ${bundle.length} `)
         return ret
