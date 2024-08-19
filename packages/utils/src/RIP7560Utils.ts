018c354a1301c37257e642a67afe20066af279ff
Alex Forshtat <forshtat1@gmail.com>
2024-08-19 19:46:06 +0200

    Fix transaction hash calculation

--- packages/utils/src/RIP7560Utils.ts
+++ packages/utils/src/RIP7560Utils.ts
@@ -29,10 +29,10 @@ function rlpEncodeRip7560Tx (op: OperationRIP7560, forSignature = true): string 
   input.push(bigNumberishToUnpaddedBuffer(op.maxPriorityFeePerGas))
   input.push(bigNumberishToUnpaddedBuffer(op.maxFeePerGas))
   input.push(bigNumberishToUnpaddedBuffer(op.callGasLimit))
-  input.push(toBuffer(op.executionData as string))
   input.push([]) // AccessList
   input.push(nonZeroAddr(op.sender))
-  input.push(toBuffer(op.authorizationData as string)) // Signature
+  input.push(toBuffer(op.authorizationData as string))
+  input.push(toBuffer(op.executionData as string))
   input.push(nonZeroAddr(op.paymaster))
   input.push(toBuffer(op.paymasterData as string))
   input.push(nonZeroAddr(op.factory))
@@ -42,8 +42,6 @@ function rlpEncodeRip7560Tx (op: OperationRIP7560, forSignature = true): string 
   input.push(bigNumberishToUnpaddedBuffer(op.paymasterVerificationGasLimit ?? 0))
   input.push(bigNumberishToUnpaddedBuffer(op.paymasterPostOpGasLimit ?? 0))
   input.push(bigNumberishToUnpaddedBuffer(op.nonceKey))
-  input.push(nonZeroAddr(undefined)) // to
-  input.push(bigNumberishToUnpaddedBuffer(0)) // value
   let rlpEncoded: any = encode(input)
   rlpEncoded = Buffer.from([4, ...rlpEncoded])
   return hexlify(rlpEncoded)
