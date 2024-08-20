ee3d35e19c4a15e7537bcc9103799cbf150e5141
Alex Forshtat <forshtat1@gmail.com>
2024-08-20 12:29:40 +0200

    AA-419: "signature" -> "authorizationData" & "calldata" -> "executionData" (#222)
    * AA-419: "signature" -> "authorizationData" & "calldata" -> "executionData"

--- packages/utils/src/interfaces/OperationRIP7560.ts
+++ packages/utils/src/interfaces/OperationRIP7560.ts
@@ -1,11 +1,15 @@
 import { OperationBase } from './OperationBase'
-import { BigNumberish } from 'ethers'
+import { BigNumberish, BytesLike } from 'ethers'
 
 export interface OperationRIP7560 extends OperationBase {
   chainId: BigNumberish
   accessList: any
   value: BigNumberish
   builderFee: BigNumberish
+
+  executionData: BytesLike
+  authorizationData: BytesLike
+
   // todo: we discussed using 'nonceKey' in the JSON schema for ERC-4337 as well but we did not finalize this decision
   nonceKey: BigNumberish
 }
