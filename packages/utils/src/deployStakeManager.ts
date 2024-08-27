09b1141da84a6fb726ef883a9070f4e8933ff82a
shahafn <shahaflol@gmail.com>
2024-08-27 14:35:36 +0300

    AA-404 Adding stake manager (#224)
    * Adding Rip7560StakeManager

---
+++ packages/utils/src/deployStakeManager.ts
@@ -0,0 +1,17 @@
+import { JsonRpcProvider } from '@ethersproject/providers'
+
+import { bytecode as stakeManagerByteCode } from '../artifacts/@account-abstraction/rip7560/contracts/predeploys/Rip7560StakeManager.sol/Rip7560StakeManager.json'
+import { DeterministicDeployer } from './DeterministicDeployer'
+import { Rip7560StakeManager__factory } from './types/factories/@account-abstraction/rip7560/contracts/predeploys'
+import { Rip7560StakeManager } from './types/@account-abstraction/rip7560/contracts/predeploys'
+
+export const stakeManagerSalt = '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3'
+
+export async function deployStakeManager (provider: JsonRpcProvider, signer = provider.getSigner()): Promise<Rip7560StakeManager> {
+  const addr = await new DeterministicDeployer(provider, signer).deterministicDeploy(stakeManagerByteCode, stakeManagerSalt)
+  return Rip7560StakeManager__factory.connect(addr, signer)
+}
+
+export function getStakeManagerAddress (): string {
+  return DeterministicDeployer.getAddress(stakeManagerByteCode, stakeManagerSalt)
+}
