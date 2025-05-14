d05e46471c94aae1a72f6607a575e6e3a9d671ff
Dror Tirosh <dror.tirosh@gmail.com>
2025-05-14 21:52:32 +0300

    lint

--- packages/validation-manager/src/ValidationManager.ts
+++ packages/validation-manager/src/ValidationManager.ts
@@ -215,7 +215,7 @@ export class ValidationManager implements IValidationManager {
       to: this.entryPoint.address,
       data,
       authorizationList: userOp.eip7702Auth == null ? null : [userOp.eip7702Auth],
-      gas: sum(prevg, userOp.verificationGasLimit, userOp.paymasterVerificationGasLimit).toNumber(),
+      gas: sum(prevg, userOp.verificationGasLimit, userOp.paymasterVerificationGasLimit).toNumber()
     }
 
     try {
